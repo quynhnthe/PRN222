91777e084161c12e1b43406784986923e0d7a1dc
quynh2003 <ntquynh.work@gmail.com>
2025-01-17 02:25:30 +0700

    First commit

---
+++ DemoTask/Program.cs
@@ -0,0 +1,57 @@
+﻿
+//Demo01
+//class Program
+//{
+//    static void PrintNumber(string message)
+//    {
+//        for (int i = 0; i < 5; i++)
+//        {
+//            Console.WriteLine($"{message} : {i}");
+//            Thread.Sleep(1000);
+//        }
+//    }
+//    static void Main()
+//    {
+//        Thread.CurrentThread.Name = "main";
+//        Task task01 = new Task(() => PrintNumber("Task 01"));
+//        task01.Start();
+//        Task task02 = Task.Run(delegate { PrintNumber("Task 02"); });
+//        Task task03 = new Task(new Action(() => { PrintNumber("Task 03"); }));
+//        task03.Start();
+//        Console.WriteLine($"Thread '{Thread.CurrentThread.Name}");
+//        Console.ReadKey();
+
+//    }
+//}
+
+//Demo02
+class Program
+{
+    
+    static void Main()
+    {
+        Task[] tasks = new Task[5];
+        String taskData = "Hello";
+        for (int i = 0; i < 5; i++) {
+            tasks[i] = Task.Run(() =>
+            {
+                Console.WriteLine($"Task = {Task.CurrentId}, obj = {taskData}, " +
+                    $"ThreadId = {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(1000);
+            });
+        }
+        try
+        {
+            Task.WaitAll(tasks);
+        } catch (AggregateException ex) {
+            Console.WriteLine("One more ex: ");
+            foreach (var e in ex.Flatten().InnerExceptions) Console.WriteLine("{0}", e.Message);
+        }
+        Console.WriteLine("Status of completed tasks");
+        foreach(var t in tasks)
+        {
+            Console.WriteLine("Task #{0} : {1}", t.Id, t.Status);
+        }
+
+    }
+}
---
+++ ServerClientApp/ClientApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Sockets;
+
+class Program
+{
+    static void ConnectServer(String server, int port)
+    {
+        string message, responseData;
+        int bytes;
+        try
+        {
+            TcpClient client = new TcpClient(server, port);
+            Console.Title = "Client Application";
+            NetworkStream stream = null;
+            while (true)
+            {
+                Console.WriteLine("Input message <press Enter to exit>:");
+                message = Console.ReadLine();
+                if (message == string.Empty)
+                {
+                    break;
+                }
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes($"{message}");
+                stream = client.GetStream();
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Sent: {0}", message);
+                data = new byte[256];
+                bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+            }
+            client.Close();
+        } catch (Exception ex)
+        {
+            Console.WriteLine("Exception: {0}", ex.Message);
+        }
+    }
+    static void Main(String[] args)
+    {
+        string server = "127.0.0.1";
+        int port = 13000;
+        ConnectServer(server, port);
+    }
+}
